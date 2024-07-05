dd8a3781a06e6f099259a576b26da26bbae27211
Krishna Panwar <krishnapanwar201@gmail.com>
2024-07-05 17:13:00 +0530

    just test for jenkins with docker

---
+++ src/main/java/com/exampledocker/demodoker/DemodokerApplication.java
@@ -0,0 +1,13 @@
+package com.exampledocker.demodoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampledocker/demodoker/DemodokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampledocker.demodoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
